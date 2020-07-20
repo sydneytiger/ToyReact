8daa2549293f7e9306dd86e467a98090ccd69640
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-07-20 23:01:38 +1000

    pluming createElement

---
+++ ToyReact.js
@@ -0,0 +1,5 @@
+export const ToyReact = {
+  createElement(){
+    debugger;
+  }
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+import {ToyReact} from './ToyReact'
+
+class MyComponent{
+
+}
+
+let a = <MyComponent name="a" />
