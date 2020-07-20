be98dec6de4d323f29c96900dd45cd053d391e0c
Tiger Chen <tigerchen@Tigers-MBP-2.modem>
2020-07-21 00:13:02 +1000

    implemented ToyReact createElement and render

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-import {ToyReact} from './ToyReact'
+import {ToyReact, Component} from './ToyReact'
 
-class MyComponent{
-
+class MyComponent extends Component{
+  render() {
+    return <div>
+            <span>Hello</span>
+            <span>world</span>
+            <div>
+              {this.children}
+            </div>
+          </div>
+  }
 }
 
-let a = <MyComponent name="a" />
+let a = <MyComponent name="a" id="ida">
+          <div>123</div>
+        </MyComponent>
+
+console.log(a);
+
+ToyReact.render(a, document.body)
